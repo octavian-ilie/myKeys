33fa009aec482277dd745c3fd96933386cbeef66
octavian-ilie <octavian.ilie@icloud.com>
2020-05-28 12:53:56 +0200

    fix: Correct key info displayed on Spacebar hit.

--- assets/js/myKeys-app.js
+++ assets/js/myKeys-app.js
@@ -6,8 +6,14 @@ const keyCodeContainer = document.querySelector('.key-code');
 const keyPressedContainer = document.querySelector('.key');
 
 function displayCode(e) {
+  let displayedKey = e.key;
+   
+  if (e.keyCode === 32) {
+    displayedKey = 'Spacebar';
+  }
+
   keyCodeContainer.textContent = e.keyCode;
-  keyPressedContainer.textContent = e.key;
+  keyPressedContainer.textContent = displayedKey;
   keyPressedContainer.style.setProperty('display', 'block');
 }
 
